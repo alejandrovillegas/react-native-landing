fe6de86330b4a5fa73cef9ff88c375f47055cb4d
Alejandro Villegas <neopilatos@gmail.com>
2018-02-22 23:04:19 -0500

    Test Components and install flux route

--- App.js
+++ App.js
@@ -8,63 +8,27 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  FlatList
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Scene, Router} from 'react-native-router-flux'
+
 
 type Props = {};
-import Icon from 'react-native-vector-icons/Ionicons';
-import Artistbox from './components/ArtistBox';
-import ArtistList from './components/ArtistList'
-import { getArtists } from './components/api-client';
-
+import HomeView from './components/HomeView'
+import ArtistDetail from './components/ArtistDetailView'
 
 
 export default class App extends Component<Props> {
-  state = {
-    artists: []
-  }
-
-  componentDidMount() {
-    getArtists()
-      .then(data => this.setState({ artists: data }))
-  }
-
   render() {
-    // const artist = {
-    //   image: 'https://lastfm-img2.akamaized.net/i/u/300x300/31a51f6e3ec647c8997150ec837891c7.png',
-    //   name: 'David Bowie',
-    //   likes: 200,
-    //   comments: 140
-    // }
-    // const artists = Array(50).fill(artist)
+    const isAndroid = Platform.OS === 'android'
 
-    const artists = this.state.artists;
-
-    return (
-      <View style={styles.container}>
-        <ArtistList artists={artists} />
-      </View>
-    );
+    return <Router>
+      <Scene key="root">
+        <Scene key="home" component={HomeView} hideNavBar />
+        <Scene key="artistDetail" component={ArtistDetail} hideNavBar={isAndroid} />
+      </Scene>
+    </Router>
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightgray',
-    paddingTop: 50,
-  }
-});
 
 AppRegistry.registerComponent('almundoApp', () => almundoApp);
--- components/ArtistList.js
+++ components/ArtistList.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   ListView,
+  TouchableOpacity,
 } from 'react-native';
 
 import ArtistBox from './ArtistBox'
+import { Actions } from 'react-native-router-flux'
 
 export default class ArtistList extends Component {
 
@@ -34,12 +36,20 @@ export default class ArtistList extends Component {
     })
   }
 
+  handlePress(artist) {
+    Actions.artistDetail({ artist })
+  }
+
   render() {
     return (
       <ListView
         enableEmptySections={true}
         dataSource={this.state.dataSource}
-        renderRow={(artist) => <ArtistBox artist={artist} />}
+        renderRow={(artist) => {
+          return <TouchableOpacity onPress={() => this.handlePress(artist)}>
+            <ArtistBox artist={artist} />
+          </TouchableOpacity>
+        }}
       />
     );
   }
